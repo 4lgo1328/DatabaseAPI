afbdcdf248e27786ade653a48af30cf4df23a0d2
algo <ekcnert1337@gmail.com>
2025-06-29 05:31:49 +0300

    Update settings.py

--- app/core/settings.py
+++ app/core/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: str
-    admin_secret_key: str
+    admin_secret_key: str = "553b0b2b-7b93-4f07-8b1d-b75896883503"
     algorithm: str
     access_token_expire_minutes: int
 
