d8f7ef28b4da434bc321f48c80bf0700e58979dd
ekcne <ekcnert1337@icloud.com>
2025-07-03 07:04:49 +0300

    minor bugfixes

--- app/schemas/payment.py
+++ app/schemas/payment.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 from decimal import Decimal
+from app.models.enums import PlanTime
 
 from app.models.enums import PaymentStatus, PlanType
 
@@ -10,7 +11,7 @@ class PaymentCreate(BaseModel):
     user_telegram_id: int
     payment_txn_id: str
     plan: PlanType
-    plan_hrs: int
+    plan_hrs: PlanTime
     amount: Decimal
     status: Optional[PaymentStatus] = PaymentStatus.pending
 
@@ -22,7 +23,7 @@ class PaymentUpdateStatusByTxn(BaseModel):
 class PaymentUpdatePlanByTxn(BaseModel):
     payment_txn_id: str
     new_plan: PlanType
-    plan_hrs: int
+    plan_hrs: PlanTime
 
 class PaymentTxn(BaseModel):
     payment_txn_id: str
@@ -35,7 +36,7 @@ class PaymentRead(BaseModel):
     amount: Decimal
     status: PaymentStatus
     plan: PlanType
-    plan_hrs: int
+    plan_hrs: PlanTime
     payment_date: datetime
 
     class Config:
